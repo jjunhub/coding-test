5534ceff23eaed96680e7061c552f72b2469158f
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2023-09-08 19:37:34 +0900

    [Easy] Time: 56 ms (95.79%), Memory: 17.6 MB (50.70%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,21 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        new_nums = sorted(nums)
-        i = 0
-        j = len(nums) - 1
+        _dict = {}
         results = []
-        while (True):
-            hap = new_nums[i] + new_nums[j]
-            if(hap == target):
-                if(nums.index(new_nums[i]) == nums.index(new_nums[j])) :
-                    results.append(nums.index(new_nums[i]))
-                    nums.remove(new_nums[i])
-                    results.append(nums.index(new_nums[j]) + 1)
-                else :
-                    results.append(nums.index(new_nums[i]))
-                    results.append(nums.index(new_nums[j]))
-                return results
-            elif(hap > target) :
-                j -= 1
+        for number in nums:
+            if number in _dict :
+                _dict[number] += 1
             else :
-                i += 1
+                _dict[number] = 1
+        
+        for index, number in enumerate(nums):
+            if target - number in _dict:
+                if target == number * 2 :
+                    if _dict[number] >= 2 :
+                        results.append(index)
+                else :
+                    results.append(index)
+                    
+        return results
