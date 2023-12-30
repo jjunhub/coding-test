ae6d6ed1279ac637f8ffb8f5695a5269c0c90db5
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2023-12-31 04:41:37 +0900

    [Medium] Time: 66 ms (5.42%), Memory: 17.7 MB (8.79%) - LeetHub

---
+++ LeetCode/Medium/0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters = []
+        digits = []
+        for log in logs:
+            identifier = log.split()[1]
+            if identifier.isdigit():
+                digits.append(log)
+            else :
+                letters.append(log)
+        result = sorted(letters, key = lambda x : (x.split()[1:], x.split()[0]))
+        result.extend(digits)
+        return result
