7d2b3a340de7873d44bb42158713ceaa404c2a4c
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2023-12-31 19:59:35 +0900

    [Medium] Time: 566 ms (53.03%), Memory: 17.2 MB (29.95%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,30 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
         longestWord = []
         for mid in range(len(s)):
-            left = mid
-            right = mid
-            while left >= 0 and right <= len(s)-1 and s[left] == s[right]:
-                left -= 1
-                right += 1
-            word = s[left+1 : right]            
-            if len(longestWord) <= len(word):
-                longestWord = word
-                
-            left = mid
-            right = mid + 1
-            while left >= 0 and right <= len(s)-1 and s[left] == s[right]:
-                left -= 1
-                right += 1
-            word = s[left+1 : right]            
-            if len(longestWord) <= len(word):
-                longestWord = word
-                
+            for case in range(2):
+                # when case = 0, start with one char. ex) a -> bab -> ...
+                # when case = 1, start with two char. ex) bb -> abba -> ...
+                left = mid
+                right = mid + case
+                while left >= 0 and right <= len(s)-1 and s[left] == s[right]:
+                    left -= 1
+                    right += 1
+                word = s[left+1 : right]            
+                if len(longestWord) <= len(word):
+                    longestWord = word
+                    
         return longestWord
                 
             
-            
+        
             
         
             
