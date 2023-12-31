2710c7927bf9d483e2aaa92ecd3f233d4bb26f5e
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2023-12-31 19:52:22 +0900

    [Medium] Time: 318 ms (90.11%), Memory: 17.3 MB (27.33%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,30 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
-        longestWord = s[0]
-        wordLen = 2
-        
-        while wordLen <= len(s):
-            print(wordLen)
-            for leftPtr in range( len(s) - wordLen + 1):
-                startleftPtr = leftPtr
-                rightPtr = leftPtr + wordLen - 1
+        longestWord = []
+        for mid in range(len(s)):
+            left = mid
+            right = mid
+            while left >= 0 and right <= len(s)-1 and s[left] == s[right]:
+                left -= 1
+                right += 1
+            word = s[left+1 : right]            
+            if len(longestWord) <= len(word):
+                longestWord = word
                 
+            left = mid
+            right = mid + 1
+            while left >= 0 and right <= len(s)-1 and s[left] == s[right]:
+                left -= 1
+                right += 1
+            word = s[left+1 : right]            
+            if len(longestWord) <= len(word):
+                longestWord = word
                 
-                while leftPtr <= rightPtr and s[leftPtr] == s[rightPtr]:
-                    leftPtr += 1
-                    rightPtr -= 1
-                    
-                if leftPtr > rightPtr:
-                    longestWord = s[startleftPtr: startleftPtr+wordLen]
-                    print(longestWord)
-                    break
-                    
-            
-            wordLen += 1
-            
         return longestWord
-                    
+                
+            
             
             
         
