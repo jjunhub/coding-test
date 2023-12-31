5ffdc21dda3cad6ef740b29d53d8de0a9eebea17
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2023-12-31 16:13:10 +0900

    [Medium] Time: 169 ms (5.68%), Memory: 22.3 MB (17.77%) - LeetHub

---
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dct = {}
+        for idx, el in enumerate(strs):
+            sortedStr = str(sorted(el))
+            
+            if sortedStr in dct:
+                dct[sortedStr].append(idx)
+            else :
+                dct[sortedStr] = [idx]
+        
+        sortedDict = sorted(dct.items(), key = lambda x : len(x[1]))
+        
+        result = [ ]
+        for el in sortedDict:
+            tempResult = []
+            for val in el[1]:
+                tempResult.append(strs[val])
+            result.append(tempResult)
+        return result
