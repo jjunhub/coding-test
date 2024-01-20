d878055e6b3d82b5a84a01ef1efb1fd4a43e7e25
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2024-01-20 20:12:49 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,25 +1,39 @@
 import collections
+def getTwoBig(oneGenreList, newSongPlayTime, idx):
+    if len(oneGenreList) < 3 :
+        oneGenreList.append((newSongPlayTime, idx))
+    else :
+        smallIdx = 1
+        smallPlays = oneGenreList[1][0] 
+        if oneGenreList[2][0] < smallPlays:
+            smallIdx = 2
+            smallPlays = oneGenreList[2][0]
+            
+        if smallPlays < newSongPlayTime:
+            oneGenreList[smallIdx] = (newSongPlayTime, idx)
+        
+    
 def solution(genres, plays):
     answer = []
-    genreDict = collections.defaultdict()
+    _dict = collections.defaultdict(list)
     
-    for i, (genre,play) in enumerate(zip(genres, plays)):
-        if genre in genreDict:
-            genreDict[genre][0] += play
-            genreDict[genre].append((i, play))
-        else:
-            genreDict[genre] = [play]
-            genreDict[genre].append((i, play))
-            
-    sortedDict = sorted(genreDict.items(), key = lambda x:x[1], reverse=True) 
-    
-    for i in sortedDict:
-        tempList = sorted(i[1][1:],  key = lambda x:x[1], reverse = True)
-        print(tempList)
-        if len(tempList) < 2 :
-            answer.append(tempList[0][0])
+    for idx in range(len(genres)):
+        genre = genres[idx]
+        playTime = plays[idx]
+        
+        if len(_dict[genre]) == 0:
+            _dict[genre] = [playTime] # total music PlayTime
+            _dict[genre].append((playTime, idx))
         else :
-            answer.append(tempList[0][0])
-            answer.append(tempList[1][0])
+            _dict[genre][0] += playTime
+            getTwoBig(_dict[genre], playTime, idx)
+            
+    sortedDict = sorted(_dict.values(), key = lambda x : x[0], reverse=True)
+    
+    for oneGenre in sortedDict:  
+        sortedOneGenre = sorted(oneGenre[1:], key = lambda x : (x[0], -x[1]), reverse = True)
+    
+        for playNum, idx in sortedOneGenre:
+            answer.append(idx)
             
     return answer
