a53b0886c95c889dd0753bbba48bfe1e3fdd9f79
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2024-01-25 00:23:31 +0900

    [level 2] Title: 프로세스, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,12 +1,9 @@
 from collections import deque 
 def solution(priorities, location):
-    processes = deque()
     answer = 0
-    for pri in priorities:
-        processes.append(pri)
+    processes = deque(priorities)
         
     while processes:
-        
         if max(processes) == processes[0]:
             processes.popleft()
             answer += 1
@@ -14,7 +11,5 @@ def solution(priorities, location):
                 return answer
         else :
             processes.rotate(-1)
-            
-        location = location - 1 if location > 0 else len(processes) - 1
         
-    return answer
+        location = location - 1 if location > 0 else len(processes) - 1
