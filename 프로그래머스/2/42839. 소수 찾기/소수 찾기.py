d6c815db3dde2dd747c40e37cac89aedeef66b5e
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2024-02-02 14:23:28 +0900

    [level 2] Title: 소수 찾기, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -17,7 +17,7 @@ def isPrimeNum(num):
     if num < 2:
         return False
     
-    for i in range(2, num):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
     return True
