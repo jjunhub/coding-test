9b0bf847b3a065721093ebc0dee2140501c4d1aa
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2024-02-02 14:22:28 +0900

    [level 2] Title: 소수 찾기, Time: 1.30 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+def solution(numbers):
+    answer = set()
+    # make all number
+    numbers = list(numbers)
+    print(numbers)
+    
+    for length in range(1, len(numbers)+1):
+        for numberStrings in permutations(numbers, length):
+            num = toIntType(numberStrings)
+            if isPrimeNum(num):
+                answer.add(num)
+
+    return len(answer)
+
+def isPrimeNum(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+def toIntType(numberStrings):
+    num = ""
+    for numberString in numberStrings:
+        num += numberString
+    return int(num)
+    
