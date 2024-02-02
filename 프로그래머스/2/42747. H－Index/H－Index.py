283ae841dca5828c311d8f356034942c5a03f854
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2024-02-02 16:01:28 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,27 +1,21 @@
 def solution(citations):
     h, idx = 0, 0
     citations.sort()
-    print(citations)
     
     while True:
-        print(f'h = {h}, idx = {idx}, citations[{idx}] = {citations[idx]} ')
-        
         # Find Where is H in citations
         if h > citations[idx]:
             while h > citations[idx]:
                 idx += 1
                 if idx == len(citations):
                     break
-                print(f'h = {h}, idx = {idx}, citations[{idx}] = {citations[idx]} ')
-            
+                    
         # Check citations number that >= h
         if h <= len(citations) - idx:
             h += 1
         else:
             break
-    return h-1
-
-print('---')
-print(solution([3,3,3,4]))
-print('---')
+    
+    # Get Max h
+    return h - 1
 
