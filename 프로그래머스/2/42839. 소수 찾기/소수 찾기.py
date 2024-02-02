56c2544b2fc85ba0a95686c875d576b5a441d786
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2024-02-02 14:37:37 +0900

    [level 2] Title: 소수 찾기, Time: 0.39 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,17 +1,18 @@
 from itertools import permutations
 def solution(numbers):
     answer = set()
-    # make all number
     numbers = list(numbers)
-    print(numbers)
     
     for length in range(1, len(numbers)+1):
+        
+        # Make All Combination
         for numberStrings in permutations(numbers, length):
-            num = toIntType(numberStrings)
-            if isPrimeNum(num):
+            num = toIntType(numberStrings) # String List to One Integer
+            
+            if isPrimeNum(num): # Check the number is Prime Number
                 answer.add(num)
 
-    return len(answer)
+    return len(answer) # Count Numbers
 
 def isPrimeNum(num):
     if num < 2:
