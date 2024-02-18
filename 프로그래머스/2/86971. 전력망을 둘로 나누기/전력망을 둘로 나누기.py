e714dcc66a83c361184e45d5bf5ef424227c4d71
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2024-02-18 20:07:39 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.98 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -11,12 +11,27 @@ def solution(n, wires):
             graph[start].append(end)
         if start not in graph[end]:
             graph[end].append(start)
-            
+
+    def bfs(root, start, end):
+        visited = [root]
+        q = deque([root])
+        while q:
+            cur_top = q.popleft()
+            for top in graph[cur_top]:
+                if start == cur_top and end == top:
+                    continue
+                if end == cur_top and start == top:
+                    continue     
+                if top not in visited:
+                    visited.append(top)
+                    q.append(top)
+        return visited
+
     
     for start, end in wires:
-        firstGraphVisited = bfs(1, graph, start, end)
+        firstGraphVisited = bfs(1, start, end)
         secondRoot = findNotVisitedTopNum(firstGraphVisited, n)
-        secondGraphVisited = bfs(secondRoot, graph, start, end)
+        secondGraphVisited = bfs(secondRoot, start, end)
         
         firstTopNum = len(firstGraphVisited)
         secondTopNum = len(secondGraphVisited)
@@ -27,20 +42,6 @@ def solution(n, wires):
         
     return answer
     
-def bfs(root, graph, start, end):
-    visited = [root]
-    q = deque([root])
-    while q:
-        cur_top = q.popleft()
-        for top in graph[cur_top]:
-            if start == cur_top and end == top:
-                continue
-            if end == cur_top and start == top:
-                continue     
-            if top not in visited:
-                visited.append(top)
-                q.append(top)
-    return visited
 
 def findNotVisitedTopNum(visited, n):
     for idx in range(1, n+1):
