465c1b6554054bbc761601ee97de23ea02a4a3d9
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2024-03-01 22:44:18 +0900

    [level 3] Title: 아이템 줍기, Time: 0.45 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,15 +1,16 @@
 from collections import deque
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    # 직사각형 중 어떤 직사각형의 변 위에 존재하는 지를 파악하는 함수
-    def getRectangleList(point):
+    # 직사각형의 변 위에 존재하는 지를 파악하는 함수
+    def isOnRectangle(point):
         point_x, point_y = point
-        rec_list = []
-        for rec_num, rec in enumerate(rectangle):
+        for rec in rectangle:
             x1, y1, x2, y2 = rec
-            if ((x1 == point_x or x2 == point_x) and y1 <= point_y <= y2) or ((y1 == point_y or y2 == point_y) and x1 <= point_x <= x2):
-                rec_list.append(rec_num)
-        return rec_list
+            if (x1 == point_x or x2 == point_x) and y1 <= point_y <= y2:
+                return True
+            if (y1 == point_y or y2 == point_y) and x1 <= point_x <= x2:
+                return True
+        return False
     
     # 어떠한 직사각형의 내부에 존재하는 지 파악하는 함수
     def isInRectangle(point):
@@ -30,32 +31,28 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
     def bfs(start, end):
         queue = deque()
         queue.append((start, 0))
-        visited = set()
-        visited.add(start)
-
+        visited = {start}
+        
         vectors = [(0,0.5), (0,-0.5), (0.5, 0), (-0.5,0)]
 
         while queue:
             now_point, count = queue.popleft()
-            print(now_point)
+            
+            # 도착지점이라면 움직인 횟수를 반환한다.
             if now_point == end:
                 return count
             
-            hubos = []
-            # 현재 위치에서 상,하,좌,우로 이동할 수 있는 지 체크한다.
-            before = getRectangleList(now_point)
+            # 현재 위치에서 상,하,좌,우로 0.5씩 이동할 수 있는 지 체크한다.
             for vec in vectors:
                 check_point = (now_point[0] + vec[0], now_point[1] + vec[1])
-                # 움직인 지점이 직사각형들의 변에 해당되는지를 확인한다.
-                after = getRectangleList(check_point)
-                # 동일한 사각형에서 이동한 것인지 확인한다. -> 폭이 1개짜리인거 점프하는거 체크 못함
-                if checkSameRectangle(before, after):
-                    if not isInRectangle(check_point):
-                        check_point = (now_point[0] + vec[0], now_point[1] + vec[1])
-                        # 변에 해당된다면, visited에 해당되는지 않는지도 확인한다.
-                        if check_point not in visited:
-                            visited.add(check_point)
-                            queue.append((check_point, count+1))
+                
+                # 움직인 지점이 직사각형들의 변에 해당되는지, 직사각형의 내부에 존재하는 지 확인한다.
+                if isOnRectangle(check_point) and not isInRectangle(check_point):
+                    
+                    # 변에 해당된다면, visited에 해당되는지 않는지도 확인한다.
+                    if check_point not in visited:
+                        visited.add(check_point)
+                        queue.append((check_point, count+0.5))
 
     answer = bfs((characterX, characterY), (itemX, itemY))
-    return answer // 2
+    return answer
