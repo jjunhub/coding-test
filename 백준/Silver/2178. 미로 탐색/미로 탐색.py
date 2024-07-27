900748288b17fc5875363d3d688bf6029057efec
Sangjun Park <107465938+jjunhub@users.noreply.github.com>
2024-07-27 09:54:30 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,47 +1,43 @@
-from collections import deque
 import sys
-
+from collections import deque
 input = sys.stdin.readline
-N, M = map(int, input().split())
-# N is y, M is x
 
-miro = [ [ ] for _ in range(N)]
+def bfs(start) -> int:
+  queue = deque()
+  queue.append(start) # ( (행, 열), 거리 )
+  dir = [ (1,0), (-1,0), (0, -1), (0, 1) ] # 상하좌우
 
-for y in range(N):
-  garo = input().rstrip()
-  for val in garo:
-    miro[y].append(int(val))
+  while queue:
+    cur_v_hang, cur_v_yeol, distance = queue.popleft()
 
-def bfs(start_v):
-  visited = set()
-  visited.add(start_v)
-  q = deque([])
-  q.append((start_v, 1))
-
-  while q :
-    cur_v, distance = q.popleft()
-    x, y = cur_v
-
-    if x == N - 1 and y == M - 1:
+    if cur_v_hang == hang-1 and cur_v_yeol == yeol-1:
       return distance
+
+    for idx in range(4):
+      next_v_hang, next_v_yeol = cur_v_hang + dir[idx][0], cur_v_yeol + dir[idx][1]
     
-    # 상
-    if y+1 < M and (x, y+1) not in visited and miro[x][y+1] == 1:
-      q.append(((x, y+1), distance + 1))
-      visited.add((x, y+1))
+      # matrix 범위를 벗어난 경우
+      if not ( 0 <= next_v_hang < hang and 0 <= next_v_yeol < yeol ):
+        continue
     
-    # 하
-    if y-1 >= 0 and (x, y-1) not in visited and miro[x][y-1] == 1:
-      q.append(((x, y-1), distance + 1))
-      visited.add((x, y-1))
+      if matrix[next_v_hang][next_v_yeol] == "1":
+        queue.append((next_v_hang, next_v_yeol, distance + 1))
+        matrix[next_v_hang][next_v_yeol] = "0"
 
-    # 좌
-    if x-1 >= 0 and (x-1, y) not in visited and miro[x-1][y] == 1:
-      q.append(((x-1,y), distance + 1))      
-      visited.add((x-1,y))
 
-    if x+1 < N and (x+1, y) not in visited and miro[x+1][y] == 1:
-      q.append(((x+1,y), distance + 1))      
-      visited.add((x+1, y))
+hang, yeol = map(int, input().split())
 
-print(bfs((0,0)))
+matrix = []
+
+for _ in range(hang):
+  # 문자열로 입력
+  line_str = input().rstrip()
+  line_li = []
+  
+  # 리스트로 변환
+  for el in line_str:
+    line_li.append(el)
+  
+  matrix.append(line_li)
+
+print(bfs((0, 0, 1)))
