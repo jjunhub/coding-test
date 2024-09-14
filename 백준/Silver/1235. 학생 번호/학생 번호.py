f6dd59d296007b429fe7225248501a8e576eebd0
Sangjun Park <sangjun1389@ajou.ac.kr>
2024-09-14 16:45:28 +0900

    [Silver IV] Title: 학생 번호, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1235. 학생 번호/학생 번호.py
+++ 백준/Silver/1235. 학생 번호/학생 번호.py
@@ -2,23 +2,19 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 numbers = []
-
 for _ in range(N):
   number = input().rstrip()
   numbers.append(number[::-1])
 
 numberLen = len(numbers[0])
 for jaritsu in range(1, numberLen+1):
-  numberSet = list()
-
+  numberSet = set()
   for number in numbers:
     new_number = number[:jaritsu]
-
     if new_number in numberSet:
       break
     else :
-      numberSet.append(new_number)
-  
+      numberSet.add(new_number)
   if len(numberSet) == N:
     break
 print(jaritsu)
