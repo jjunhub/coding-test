c1052970c5fb88159fc50c9beb69a5c06521ed9f
Sangjun Park <sangjun1389@ajou.ac.kr>
2024-12-09 04:00:01 +0900

    [Gold III] Title: 파티, Time: 1112 ms, Memory: 36584 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from heapq import heappop, heappush
+
+def dijkstra(start_node, end_node):
+  visited = set()
+  q = list()
+  heappush(q, (0, start_node))
+
+  while q:
+    total_cost, now_node = heappop(q)
+    if now_node == end_node:
+      return total_cost
+    
+    if now_node in visited:
+      continue
+    visited.add(now_node)
+
+    for next_node, cost in graph[now_node]:
+      if next_node not in visited:
+        heappush(q, (total_cost+cost, next_node))
+
+graph = defaultdict(list)
+node_number, edge_number, party_node = map(int, input().split())
+for _ in range(edge_number):
+  start_node, end_node, cost = map(int, input().split())
+  graph[start_node].append((end_node, cost))
+
+answer = -1
+for home_node in range(1, node_number+1):
+  answer = max(answer, dijkstra(home_node, party_node) + dijkstra(party_node, home_node))
+
+print(answer)
