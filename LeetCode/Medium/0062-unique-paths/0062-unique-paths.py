b01628d6d307caa494a62ac9b157bf71c564c8cf
Sangjun Park <sangjun1389@ajou.ac.kr>
2025-01-07 23:43:18 +0900

    Time: 0 ms (100%), Memory: 17.9 MB (8.8%) - LeetHub

---
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,14 @@
+from collections import deque
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        matrix = [[ 0 for _ in range(n)] for _ in range(m)]
+        matrix[0][0] = 1
+
+        for row in range(m):
+            for col in range(n):
+                if col-1 >= 0:
+                    matrix[row][col] += matrix[row][col-1]
+                if row-1 >= 0:
+                    matrix[row][col] += matrix[row-1][col]
+        
+        return matrix[m-1][n-1]
