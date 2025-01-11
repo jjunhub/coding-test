f335ff5d501a61c21fea3a9923839b46defb0235
Sangjun Park <sangjun1389@ajou.ac.kr>
2025-01-11 12:06:17 +0900

    [Bronze II] Title: 주사위, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1233. 주사위/주사위.py
+++ 백준/Bronze/1233. 주사위/주사위.py
@@ -1,18 +1,7 @@
-from collections import defaultdict
-S1, S2, S3 = map(int, input().split())
-result = defaultdict(int)
-
-for s1 in range(1, S1+1):
-  for s2 in range(1, S2+1):
-    for s3 in range(1, S3+1):
-      result[s1+s2+s3] += 1
-
-max_value = -1
-answer = 0
-
-for key, value in result.items():
-  if max_value < value:
-    max_value = value
-    answer = key
-
-print(answer)
+s1,s2,s3 = map(int,input().split())
+bindo = [0] * 81
+for i in range(1, s1+1):
+  for j in range(1, s2+1):
+    for k in range(1, s3+1):
+        bindo[i +j+k] += 1
+print(bindo.index(max(bindo)))
