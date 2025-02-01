973890aa78f89d843d34bbfdeae352e2a94cff81
Sangjun Park <sangjun1389@ajou.ac.kr>
2025-02-01 12:44:15 +0900

    [Silver II] Title: 지구 온난화, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5212. 지구 온난화/지구 온난화.py
+++ 백준/Silver/5212. 지구 온난화/지구 온난화.py
@@ -1,35 +1,28 @@
 R, C = map(int, input().split())
-matrix = [ input() for _ in range(R)]
-answer = matrix[:]
-min_col = 11
-max_col = -1
-min_row = 11
-max_row = -1
+matrix = [ list(input()) for _ in range(R)]
+answer = [ line[:] for line in matrix ]
+max_col, min_col = -1, C
+max_row, min_row = -1, R
+mv = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 for row in range(R):
   for col in range(C):
-    if matrix[row][col] == "X":
+    if matrix[row][col] == 'X':
       seaCount = 0
-      # 상
-      if row - 1 < 0 or matrix[row-1][col] == ".":
-        seaCount += 1
-      # 하
-      if row + 1 >= R or matrix[row+1][col] == ".":
-        seaCount += 1
-      # 좌
-      if col - 1 < 0 or matrix[row][col-1] == ".":
-        seaCount += 1
-      # 우
-      if col + 1 >= C or matrix[row][col+1] == ".":
-        seaCount += 1
+      for dr, dc in mv:
+        next_row, next_col = row + dr, col + dc
 
-      if seaCount < 3:
+        if not ( 0 <= next_row < R and 0 <= next_col < C) or matrix[next_row][next_col] == ".":
+          seaCount += 1
+        
+      if seaCount >= 3:
+        answer[row][col] = "."
+      else :
         min_row = min(min_row, row)
         max_row = max(max_row, row)
         min_col = min(min_col, col)
         max_col = max(max_col, col)
-      else :
-        answer[row] = answer[row][:col] + "." + answer[row][col+1:]
+
 for row in range(min_row, max_row+1):
   for col in range(min_col, max_col+1):
     print(answer[row][col], end="")
