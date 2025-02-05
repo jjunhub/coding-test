a0fd79f7fd1abcbacf35697629f0e1539f0bf056
Sangjun Park <sangjun1389@ajou.ac.kr>
2025-02-06 02:56:20 +0900

    [Gold V] Title: 평범한 배낭, Time: 1180 ms, Memory: 36396 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,9 +1,12 @@
-N, K = map(int, input().split())
-items = [ tuple(map(int, input().split())) for _ in range(N)]
-dp = [0] * (K+1)
+def sol():
+  N, K = map(int, input().split())
+  items = [ tuple(map(int, input().split())) for _ in range(N)]
+  dp = [0] * (K+1)
 
-for w, v in items:
-  for j in range(K, w-1, -1):
-    dp[j] = max(dp[j], dp[j-w] + v)
+  for w, v in items:
+    for j in range(K, w-1, -1):
+      dp[j] = max(dp[j], dp[j-w] + v)
 
-print(dp[K])
+  print(dp[K])
+
+sol()
