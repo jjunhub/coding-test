471bbcabddfdbf3635b3f45c487525562b6ec2c7
Sangjun Park <sangjun1389@ajou.ac.kr>
2025-02-06 02:55:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 2708 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+items = [ tuple(map(int, input().split())) for _ in range(N)]
+dp = [0] * (K+1)
+
+for w, v in items:
+  for j in range(K, w-1, -1):
+    dp[j] = max(dp[j], dp[j-w] + v)
+
+print(dp[K])
